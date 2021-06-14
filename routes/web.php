16fba14a3fe934f904a7a181e4fdfb9c7ac639c8
Moises Freitas <moisesfreitas00174@gmail.com>
2021-06-14 14:29:45 -0300

    implementando layouts 2

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,11 @@ Route::get('/contato', function () {
 Route::get('/', 'PrincipalController@principal')->name('site.index');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::post('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/login')->name('site.login');
 
-
+Route::prefix('/app')->group(function() {
+    Route::get('/clientes')->name('app.clientes');
+    Route::get('/fornecedores')->name('app.fornecedores');
+    Route::get('/produtos')->name('app.produtos');
+});
